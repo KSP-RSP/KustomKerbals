537974051f23f039395e717ac7826f5f376d01be
Clifton M <Clifton@Cliftons-MacBook.local>
2016-03-17 22:38:44 -0400

    Finished latest update

--- KustomKerbals/KreationHandler.cs
+++ KustomKerbals/KreationHandler.cs
@@ -21,11 +21,10 @@ namespace KustomKerbals
 	[KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
 	public class KK : MonoBehaviour
 	{
-		private static Rect windowPosition = new Rect(0, 0, 370, 240);
-		private static GUIStyle windowStyle = null;
+		private static Rect windowPosition = new Rect(0, 0, 370, 265);
 		private static bool buttonState = false;
-		private static bool maleState = true;
-		private static bool femaleState = false;
+		private static bool male = true;
+		public string gender = "Male";
 		public static bool windowState = false;
 		public static bool isKSCLocked = false;
 		public string stringToEdit = "My Kustom Kerbal";
@@ -36,33 +35,42 @@ namespace KustomKerbals
 
 		public void Start()
 		{
-			//Sets window style to KSP default.
-			windowStyle = new GUIStyle(HighLogic.Skin.window);
+			
 			Debug.Log("Kustom Kerbals loaded.");
-			Debug.Log("And so begins our space adventure of cryptographic messages...");
-			Debug.Log("If you can decode them, go to the Kustom Kerbals forum thread, and say what message you have decoded...");
+
+			//Move the window to the center of the screen
 			windowPosition.x = Screen.width / 2 - windowPosition.width / 2;
 			windowPosition.y = Screen.height / 2 - windowPosition.height / 2;
 
 		}
 
-		public void OnGUI()
+		//Tells the window to open.
+		public void Update()
 		{
 
-			//Checks if player has toggled the window on or off.
-			if (windowState == true)
-			{
-				
-				windowPosition = GUI.Window(1234, windowPosition, OnWindow, "Kustom Kerbals", windowStyle);
+			if (male) {
+
+				gender = "Male";
+
+			} else {
+
+				gender = "Female";
 
 			}
 
+			if ((GameSettings.MODIFIER_KEY.GetKey()) && Input.GetKeyDown(KeyCode.K))
+			{
+				windowState = !windowState;
+
+				Debug.Log("KK window opened");
+
+			}
 		}
 
 		//Gets a new kerbal and sets his/her stats.
 		private void SpawnKerbal(int count)
 		{
-			
+
 			ProtoCrewMember kerbal = HighLogic.CurrentGame.CrewRoster.GetNewKerbal();
 			kerbal.name = stringToEdit;
 			kerbal.courage = sliderValue;
@@ -71,12 +79,12 @@ namespace KustomKerbals
 
 
 			//Find out gender
-			if (femaleState == true) {
+			if (male == false) {
 
 				kerbal.gender = ProtoCrewMember.Gender.Female;
 
 			}
-			if (maleState == true) {
+			if (male) {
 
 				kerbal.gender = ProtoCrewMember.Gender.Male;
 
@@ -87,90 +95,23 @@ namespace KustomKerbals
 			//				KerbalRoster.SetExperienceTrait(kerbal, "Engineer");
 			//				KerbalRoster.SetExperienceTrait(kerbal, "Scientist");
 		}
-		//Gets a new kerbal and sets his/her stats.
-		private void SpawnBadassKerbal(int count)
-		{
-			
-			ProtoCrewMember kerbal = HighLogic.CurrentGame.CrewRoster.GetNewKerbal();
-			kerbal.name = stringToEdit;
-			kerbal.courage = sliderValue;
-			kerbal.stupidity = sliderValue2;
-			kerbal.isBadass = true;
 
-			if (femaleState == true) {
+		#region rendering
 
-				kerbal.gender = ProtoCrewMember.Gender.Female;
-
-			}
-			if (maleState == true) {
-
-				kerbal.gender = ProtoCrewMember.Gender.Male;
-
-			}
-			
-		}
-		private void SpawnMarkKerbal(int count)
-		{
-			
-			ProtoCrewMember kerbal = HighLogic.CurrentGame.CrewRoster.GetNewKerbal();
-			kerbal.name = "Mark Watney";
-			kerbal.courage = sliderValue;
-			kerbal.stupidity = sliderValue2;
-			kerbal.isBadass = true;
-			kerbal.gender = ProtoCrewMember.Gender.Male;
-
-		}
-
-		//makes the new kerbal available for use.
-		private void SpawnKerbal(ProtoCrewMember kerbal)
+		public void OnGUI()
 		{
 
-			kerbal.rosterStatus = ProtoCrewMember.RosterStatus.Available;
+			//Set skin to KSP
+			GUI.skin = HighLogic.Skin;
 
-		}
-
-
-
-		//Tells the window to open.
-		public void Update()
-		{
-
-			if (femaleState == true) {
-
-				maleState = false;
-
-			} else {
-
-				maleState = true;
-
-			}
-			if (maleState == true) {
-
-				femaleState = false;
-
-			} else{
-
-				femaleState = true;
-
-			}
-
-
-
-
-			if ((GameSettings.MODIFIER_KEY.GetKey()) && Input.GetKeyDown(KeyCode.K))
+			//Checks if player has toggled the window on or off.
+			if (windowState == true)
 			{
-				windowState = true;
-				if (!KK.isKSCLocked)
-				{
-					
-					InputLockManager.SetControlLock(ControlTypes.KSC_FACILITIES, "KSCLock");
-					KK.isKSCLocked = true;
 
-				}
-
-				Debug.Log("KK window opened");
+				windowPosition = GUI.Window(1234, windowPosition, OnWindow, "Kustom Kerbals");
 
 			}
+
 		}
 
 		private void OnWindow(int windowID)
@@ -188,10 +129,12 @@ namespace KustomKerbals
 
 			//Toggle female/male
 			GUILayout.BeginHorizontal();
-			GUILayout.Label("Female:");
-			femaleState = GUILayout.Toggle(femaleState, "Female: " + femaleState);
-			GUILayout.Label("Male:");
-			maleState = GUILayout.Toggle(maleState, "Male: " + maleState);
+			GUILayout.Label("Toggle Male/Female: ");
+			if (GUILayout.Button (gender)) {
+
+				male = !male;
+
+			}
 			GUILayout.EndHorizontal();
 
 			//Sets kerbal's courage. 
@@ -212,16 +155,10 @@ namespace KustomKerbals
 			GUILayout.EndHorizontal();
 
 			//Button to create the kerbal using above paramaters.
-			if (GUI.Button(new Rect(7, 190, 220, 35), "Kreate Kustom Kerbal"))
-			{
-				if (isKSCLocked)
-				{
-					
-					InputLockManager.RemoveControlLock("KSCLock");
-					isKSCLocked = false;
-
-				}
-				if (stringToEdit == "Jebediah Kerman Jr." && sliderValue == 0.0f && sliderValue2 == 0.0f && buttonState == false && femaleState == false)
+			GUILayout.BeginHorizontal();
+			if (GUILayout.Button("Kreate Kustom Kerbal")) {
+				
+				if (stringToEdit == "Jebediah Kerman Jr." && sliderValue == 0.0f && sliderValue2 == 0.0f && buttonState == false && male == true)
 				{
 					
 					ProtoCrewMember kerbal = HighLogic.CurrentGame.CrewRoster.GetNewKerbal();
@@ -229,51 +166,6 @@ namespace KustomKerbals
 					windowState = false;
 					ScreenMessages.PostScreenMessage("Random Kerbal Spawned, closing window...", 1, ScreenMessageStyle.UPPER_CENTER);
 					Debug.Log("Random Kerbal Spawned");
-				}
-				else if (stringToEdit == "Badass Kerman") {
-
-					Debug.Log("Badass Kerbal Kreated.");
-					Debug.Log("KK window closed.");
-					SpawnBadassKerbal(kerbal);
-					windowState = false;
-					ScreenMessages.PostScreenMessage("Badass Kerbal Spawned, closing window...", 1, ScreenMessageStyle.UPPER_CENTER);
-
-				}
-				else if (stringToEdit == "badass kerman") {
-
-					Debug.Log("Badass Kerbal Kreated.");
-					Debug.Log("KK window closed.");
-					SpawnBadassKerbal(kerbal);
-					windowState = false;
-					ScreenMessages.PostScreenMessage("Badass Kerbal Spawned, closing window...", 1, ScreenMessageStyle.UPPER_CENTER);
-
-				}
-				else if (stringToEdit == "Badass") {
-
-					Debug.Log("Badass Kerbal Kreated.");
-					Debug.Log("KK window closed.");
-					SpawnBadassKerbal(kerbal);
-					windowState = false;
-					ScreenMessages.PostScreenMessage("Badass Kerbal Spawned, closing window...", 1, ScreenMessageStyle.UPPER_CENTER);
-
-				}
-				else if (stringToEdit == "badass") {
-
-					Debug.Log("Badass Kerbal Kreated.");
-					Debug.Log("KK window closed.");
-					SpawnBadassKerbal(kerbal);
-					windowState = false;
-					ScreenMessages.PostScreenMessage("Badass Kerbal Spawned, closing window...", 1, ScreenMessageStyle.UPPER_CENTER);
-
-				}
-				else if (stringToEdit == "Duna") {
-
-					Debug.Log("Mark Watney Kerbal Kreated.");
-					Debug.Log("KK window closed.");
-					SpawnMarkKerbal(kerbal);
-					windowState = false;
-					ScreenMessages.PostScreenMessage("Mark Watney Kerbal Spawned, let's science the s**t out of this...", 1, ScreenMessageStyle.UPPER_CENTER);
-
 				}
 				else
 				{
@@ -286,8 +178,11 @@ namespace KustomKerbals
 
 				}
 			}
+			GUILayout.EndHorizontal ();
 
 			GUI.DragWindow();
 		}
+
+		#endregion
 	}
 }
